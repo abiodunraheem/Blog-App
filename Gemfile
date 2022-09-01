3089be541f3fda8eb7c9d67a37d9988a15c3e937
abiodunraheem <raheemabiodun23@gmail.com>
2022-09-01 12:11:41 +0100

    Add confirmation to devise

--- Gemfile
+++ Gemfile
@@ -60,6 +60,7 @@ end
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem 'web-console'
+  gem 'letter_opener'
 end
 
 gem 'childprocess'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable
   has_many :posts, class_name: 'Post'
   has_many :comments, class_name: 'Comment'
   has_many :likes, class_name: 'Like'
 
-  #validates :name, presence: true
+  #validates
+  validates :name, :bio, presence: true
+  validates :email, uniqueness: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_posts
