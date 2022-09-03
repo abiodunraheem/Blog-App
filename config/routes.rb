e407f94cfcb64962ca7c5223564f89ce08ea3015
NatiG <nati.g5684@gmail.com>
2022-09-03 14:46:03 +0300

    Remove unnecassary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'users#index'
-
-  resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show new create destroy]
-  end
-
-  resources :posts do
-    resources :comments, only: %i[create destroy]
-    resources :likes, only: %i[create]
-  end
-  namespace :api do
-    namespace :v1 do
-      resources :comments, only: [:index]
-    end
-  end
-end
-
-
-Rails.application.routes.draw do
   devise_for :users,
              controllers: {
                sessions: 'users/sessions',
