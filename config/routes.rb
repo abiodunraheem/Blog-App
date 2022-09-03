c5127352e689f4c65b83fbf0a2007b55d87cf968
abiodunraheem <raheemabiodun23@gmail.com>
2022-09-03 12:41:47 +0100

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,3 +16,32 @@ Rails.application.routes.draw do
     end
   end
 end
+
+
+Rails.application.routes.draw do
+  devise_for :users,
+             controllers: {
+               sessions: 'users/sessions',
+               registrations: 'users/registrations'
+             }
+  resources :users, only: %I[index show] do
+    resources :posts, only: %I[index show new create destroy]
+  end
+
+  resources :posts do
+    resources :comments, only: %i[create destroy]
+    resources :likes, only: %i[create]
+  end
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index] do
+        resources :posts, only: [:show, :index] do
+          resources :comments, only: [:new, :create]
+        end
+      end
+    end
+  end
+
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
+end
